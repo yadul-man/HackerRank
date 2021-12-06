f30e6f9ca72760fb4267919408e59830a58787a2
yadul-man <95302854+yadul-man@users.noreply.github.com>
2021-12-06 19:49:59 +0530

    Create JavaStdinAndStdout-1.java

---
+++ Java/JavaStdinAndStdout-1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        long a = scan.nextLong();
+        long b = scan.nextLong();
+        long c = scan.nextLong();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
