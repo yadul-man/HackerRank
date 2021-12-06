2688be8c6871558fd6ba595c32c94497187f8e8c
yadul-man <95302854+yadul-man@users.noreply.github.com>
2021-12-06 19:52:18 +0530

    Update JavaStdinAndStdout-1.java

--- Java/JavaStdinAndStdout-1.java
+++ Java/JavaStdinAndStdout-1.java
@@ -1,3 +1,7 @@
+/*
+In this challenge, you must read  integers from stdin and then print them to stdout. Each integer must be printed on a new line.
+*/
+
 import java.util.*;
 
 public class Solution {
