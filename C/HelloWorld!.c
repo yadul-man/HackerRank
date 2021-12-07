12dc8be5806c669e7decddf7c7ba8b0010091b2a
yadul-man <95302854+yadul-man@users.noreply.github.com>
2021-12-07 20:27:32 +0530

    Create HelloWorld!.c

---
+++ C/HelloWorld!.c
@@ -0,0 +1,18 @@
+/*
+This challenge requires you to print Hello, World! on a single line, and then print the already provided input string to stdout. 
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+	
+    char s[100];
+    scanf("%[^\n]%*c", &s);
+  	
+    printf("Hello, World!\n%s", s);   
+    return 0;
+}
